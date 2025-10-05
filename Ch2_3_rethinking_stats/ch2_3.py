010a8a1d99286482c70bb63be62b9e860f00618d
marcelo <marcelo_mf15@hotmail.com>
2025-10-05 19:08:15 +0100

    Finished almost all medium probs ch3

--- Ch2_3_rethinking_stats/ch2_3.py
+++ Ch2_3_rethinking_stats/ch2_3.py
@@ -17,11 +17,17 @@ def _():
     import numpy as np
     import scipy as sp
     from scipy.stats import binom
+    import arviz as az
     import pymc as pm
     import altair as alt
+    import matplotlib.pyplot as plt
+    return alt, az, binom, mo, np, pl, plt
 
-    np.random.seed = 42
-    return alt, binom, mo, np, pl
+
+@app.cell
+def _(np):
+    np.random.seed(42)
+    return
 
 
 @app.cell(hide_code=True)
@@ -334,65 +340,154 @@ def _(mo):
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Exercise - Easy""")
+    return
+
+
 @app.cell
 def _(grid_approx_ch2, np, pl):
-    _size = 10_000
-    ch3_post_df, ch3_post_chart = grid_approx_ch2(_size, "uniform", success=6, tosses=9)
+    ch3_post_df, ch3_post_chart = grid_approx_ch2(1_001, "uniform", success=6, tosses=9)
 
     # samples used for a lot of the problems
-    samples = np.random.choice(
+    samples_easy = np.random.choice(
         ch3_post_df.select(pl.col("prob_water")).to_numpy().ravel(),
-        _size,
+        10_000,
         replace=True,
         p=ch3_post_df.select(pl.col("posterior_prob")).to_numpy().ravel(),
     )
-    return (samples,)
+
+    # ch3_post_chart
+    return (samples_easy,)
+
+
+@app.cell
+def _(np, samples_easy):
+    # 3E1. How much posterior probability lies below p = 0.2?
+    np.sum(samples_easy < 0.2) / samples_easy.size
+    return
+
+
+@app.cell
+def _(np, samples_easy):
+    # 3E2. How much posterior probability lies above p = 0.8?
+    np.sum(samples_easy > 0.8) / samples_easy.size
+    return
+
+
+@app.cell
+def _(np, samples_easy):
+    # 3E3. How much posterior probability lies between p = 0.2 and p = 0.8?
+    np.sum((0.2 < samples_easy) & (samples_easy < 0.8)) / samples_easy.size
+    return
+
+
+@app.cell
+def _(np, samples_easy):
+    # 3E4. 20% of the posterior probability lies below which value of p?
+    np.quantile(samples_easy, 0.2)
+    return
+
+
+@app.cell
+def _(np, samples_easy):
+    # 3E5. 20% of the posterior probability lies above which value of p?
+    np.quantile(samples_easy, 0.8)
+    return
+
+
+@app.cell
+def _(az, samples_easy):
+    # 3E6. Which values of p contain the narrowest interval equal to 66% of the posterior probability?
+
+    # The question is asking for the highest density posterior interval (HDPI or HDI)
+    az.hdi(samples_easy, hdi_prob=0.66)
+    return
+
+
+@app.cell
+def _(np, samples_easy):
+    # 3E7. Which values of p contain 66% of the posterior probability, assuming equal posterior probability both below and above the interval?
+
+    middle_pct = 0.66
+    pct_tail = (1 - middle_pct) / 2
+
+    np.quantile(samples_easy, (pct_tail, 1 - pct_tail))
+    return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""### Exercise 1 - Easy""")
+    mo.md(r"""### Exercise - Medium""")
     return
 
 
 @app.cell
-def _(np, samples):
-    # 3E1. How much posterior probability lies below p = 0.2?
-    np.sum(samples < 0.2) / samples.size
+def _(grid_approx_ch2, np, pl):
+    # 3M1. Suppose the globe tossing data had turned out to be 8 water in 15 tosses. Construct the posterior distribution, using grid approximation. Use the same flat prior as before.
+
+    ch3_m1_to_4_df, chart_ch3_m1_to_4 = grid_approx_ch2(p_grid_size=1_001, prior="step", success=11, tosses=15)
+
+    # samples used for a lot of the problems
+    samples_medium = np.random.choice(
+        ch3_m1_to_4_df.select(pl.col("prob_water")).to_numpy().ravel(),
+        10_000,
+        replace=True,
+        p=ch3_m1_to_4_df.select(pl.col("posterior_prob")).to_numpy().ravel(),
+    )
+
+    chart_ch3_m1_to_4
+    return (samples_medium,)
+
+
+@app.cell
+def _(az, samples_medium):
+    # 3M2. Draw 10,000 samples from the grid approximation from above. Then use the samples to calculate the 90% HPDI for p.
+    az.hdi(ary=samples_medium, hdi_prob=0.9)
     return
 
 
 @app.cell
-def _(np, samples):
-    # 3E2. How much posterior probability lies above p = 0.8?
-    np.sum(samples > 0.8) / samples.size
-    return
+def _(binom, np, plt, samples_medium):
+    # 3M3. Construct a posterior predictive check for this model and data. This means simulate the distribution of samples, averaging over the posterior uncertainty in p. What is the probability of observing 8 water in 15 tosses?
+
+    # posterior predictive distribution
+    ppd = np.random.binomial(n=15, p=samples_medium)
+
+    plt.hist(ppd, bins=15, label="Bayesian with step prior")
+
+    # we know that around 70% of Earth is water, so the below is the true distribution
+    plt.plot(
+        np.linspace(0, 15, 16),
+        samples_medium.size * binom.pmf(np.linspace(0, 15, 16), n=15, p=0.7),
+        label="True underlying model",
+    )
+    plt.title("Posterior predictive distribution")
+    plt.legend(loc=(1.05, 0.9))
+    plt.show()
+    return (ppd,)
 
 
 @app.cell
-def _(np, samples):
-    # 3E3. How much posterior probability lies between p = 0.2 and p = 0.8?
-    np.sum((0.2 < samples) & (samples < 0.8)) / samples.size
-    return
-
-
-@app.cell
-def _(np, samples):
-    # 3E4. 20% of the posterior probability lies below which value of p?
-    np.quantile(samples, 0.2)
-    return
-
-
-@app.cell
-def _(np, samples):
-    # 3E5. 20% of the posterior probability lies above which value of p?
-    np.quantile(samples, 0.8)
+def _(ppd):
+    # 3M4. Using the posterior distribution constructed from the new (8/15) data, now calculate the probability of observing 6 water in 15 tosses.
+    (ppd == 6).mean()
     return
 
 
 @app.cell
 def _():
-    # 3E6. Which values of p contain the narrowest interval equal to 66% of the posterior probability?
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
     return
 
 
