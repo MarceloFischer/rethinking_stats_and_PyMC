d8f1f3e5ec6ffdff70df6b2280eeb02379b91ca2
marcelo <marcelo_mf15@hotmail.com>
2026-01-17 20:27:33 +0000

    added homework pdfs

--- Ch1_2_3_rethinking_stats/marcelo_fischer_week_1.py
+++ Ch1_2_3_rethinking_stats/marcelo_fischer_week_1.py
@@ -2,26 +2,44 @@
 # requires-python = ">=3.13"
 # dependencies = [
 #     "altair==6.0.0",
-#     "marimo",
+#     "duckdb==1.4.3",
+#     "marimo>=0.19.0",
+#     "matplotlib==3.10.8",
+#     "mcp>=1",
+#     "nbformat==5.10.4",
 #     "numpy==2.4.1",
 #     "openai==2.15.0",
+#     "polars[pyarrow]==1.37.1",
+#     "pydantic>=2",
+#     "pydantic-ai==1.44.0",
 #     "pytest==9.0.2",
-#     "ruff==0.14.11",
+#     "python-lsp-ruff==2.3.0",
+#     "python-lsp-server==1.14.0",
+#     "pyzmq",
+#     "ruff==0.14.13",
+#     "sqlglot==28.6.0",
 #     "vegafusion==2.0.3",
 #     "vl-convert-python==1.9.0",
+#     "websockets==15.0.1",
 # ]
 # ///
 
 import marimo
 
-__generated_with = "0.19.2"
-app = marimo.App(width="medium")
+__generated_with = "0.19.4"
+app = marimo.App(width="columns")
 
 
 @app.cell
 def _():
     import marimo as mo
     import numpy as np
+
+    return
+
+
+@app.cell
+def _():
     return
 
 
