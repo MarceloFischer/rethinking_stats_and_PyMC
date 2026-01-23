eee15dd3694015e81ca5ad8aa2d2b24ad908d7b3
marcelo <marcelo_mf15@hotmail.com>
2026-01-23 18:41:12 +0000

    Added files and homework for week 1

--- Ch1_2_3_rethinking_stats/marcelo_fischer_week_1.py
+++ Ch1_2_3_rethinking_stats/marcelo_fischer_week_1.py
@@ -1,29 +1,3 @@
-# /// script
-# requires-python = ">=3.13"
-# dependencies = [
-#     "altair==6.0.0",
-#     "duckdb==1.4.3",
-#     "marimo>=0.19.0",
-#     "matplotlib==3.10.8",
-#     "mcp>=1",
-#     "nbformat==5.10.4",
-#     "numpy==2.4.1",
-#     "openai==2.15.0",
-#     "polars[pyarrow]==1.37.1",
-#     "pydantic>=2",
-#     "pydantic-ai==1.44.0",
-#     "pytest==9.0.2",
-#     "python-lsp-ruff==2.3.0",
-#     "python-lsp-server==1.14.0",
-#     "pyzmq",
-#     "ruff==0.14.13",
-#     "scipy==1.17.0",
-#     "sqlglot==28.6.0",
-#     "vegafusion==2.0.3",
-#     "vl-convert-python==1.9.0",
-# ]
-# ///
-
 import marimo
 
 __generated_with = "0.19.4"
---
+++ Ch1_2_3_rethinking_stats/marcelo_fischer_week_2.py
@@ -0,0 +1,27 @@
+import marimo
+
+__generated_with = "0.19.5"
+app = marimo.App(width="columns")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import polars as pl
+    import numpy as np
+    import scipy as sp
+    from scipy.stats import binom, beta
+    import arviz as az
+    import pymc as pm
+    import altair as alt
+    import matplotlib.pyplot as plt
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
