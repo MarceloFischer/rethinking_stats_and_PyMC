d2b71f823812f7bfb2fc68bfff9f2b6e087d008d
marcelo <marcelo_mf15@hotmail.com>
2026-03-16 11:52:19 +0000

    started lecture notes

--- Ch_5_rethinking_stats/Ch_5.py
+++ Ch_5_rethinking_stats/Ch_5.py
@@ -26,6 +26,7 @@ app = marimo.App(width="columns")
 
 @app.cell(column=0)
 def _():
+    import operator
     import altair as alt
     import arviz as az
     import marimo as mo
@@ -38,83 +39,288 @@ def _():
 
     RANDOM_SEED = 1523
     rng = np.random.default_rng(RANDOM_SEED)
-    az.style.use("arviz-darkgrid")
+    plt.style.use("fivethirtyeight")
 
     az.rcParams["stats.ci_prob"] = 0.89  # sets default credible interval used by arviz
-    return Path, az, np, pl, plt, pm
+    return Path, az, mo, np, operator, pl, plt, pm, rng
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Constants
+    """)
+    return
 
 
 @app.cell
 def _(Path, pl):
-    data_file_path = Path(__file__).parent.parent / "data" / "WaffleDivorce.csv"
+    #############
+    # Constants #
+    #############
+    WAFFLE_PATH = Path(__file__).parent.parent / "data" / "WaffleDivorce.csv"
+    HOWELL_PATH = Path(__file__).parent.parent / "data" / "Howell1.csv"
 
-    data = pl.read_csv(data_file_path, separator=";")
+    # women=0 and men=1 in the dataset
+    SEX = ["women", "men"]
 
-    # add standardised columns
-    data = data.with_columns(
-        [
-            ((pl.col(col) - pl.col(col).mean()) / pl.col(col).std()).alias(f"{col}_std")
-            for col in ["Divorce", "MedianAgeMarriage", "Marriage"]
-        ]
-    )
+    raw_waffle_data = pl.read_csv(WAFFLE_PATH, separator=";")
+    raw_howell_data = pl.read_csv(HOWELL_PATH, separator=";")
+    return SEX, raw_howell_data, raw_waffle_data
 
-    data
-    return (data,)
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Helper Functions
+    """)
+    return
 
 
 @app.cell
-def _(data, plt):
+def _(operator, pl):
+    def cols_to_lowercase(dataf: pl.DataFrame) -> pl.DataFrame:
+        return dataf.rename({col: col[0].lower() + col[1:] for col in dataf.columns})
+
+
+    def std_cols_of_interest(dataf: pl.DataFrame) -> pl.DataFrame:
+        # add standardised columns
+        return dataf.with_columns(
+            [
+                ((pl.col(col) - pl.col(col).mean()) / pl.col(col).std()).alias(f"{col}_std")
+                for col in ["divorce", "medianAgeMarriage", "marriage"]
+            ]
+        )
+
+
+    def filter_by_comparison(
+        df: pl.DataFrame, col_name: str, value: float, op_str: str
+    ) -> pl.DataFrame:
+        # Map string labels to python operators
+        ops = {
+            ">": operator.gt,
+            ">=": operator.ge,
+            "<": operator.lt,
+            "<=": operator.le,
+            "==": operator.eq,
+        }
+
+        # Use the operator to create a Polars expression
+        # Equivalent to: df.filter(pl.col(col_name) >= value)
+        return df.filter(ops[op_str](pl.col(col_name), value))
+
+    return cols_to_lowercase, filter_by_comparison, std_cols_of_interest
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Data Prep.
+    """)
+    return
+
+
+@app.cell
+def _(
+    cols_to_lowercase,
+    filter_by_comparison,
+    raw_howell_data,
+    raw_waffle_data,
+    std_cols_of_interest,
+):
+    waffle_data = raw_waffle_data.pipe(cols_to_lowercase).pipe(std_cols_of_interest)
+
+    howell_adults = raw_howell_data.pipe(filter_by_comparison, "age", 18, ">=")
+
+    howell_children = raw_howell_data.pipe(filter_by_comparison, "age", 13, "<=")
+
+    # raw_howell_data, raw_waffle_data
+    return howell_adults, waffle_data
+
+
+@app.cell
+def _(pl, plt, raw_howell_data):
+    def _() -> plt.Axes:
+        # 1. Create the boolean expression/mask
+        is_adult = pl.col("age") >= 18
+
+        # 2. Apply filtering
+        adult_howell = raw_howell_data.filter(is_adult)
+        child_howell = raw_howell_data.filter(~is_adult)
+
+        plt.scatter(adult_howell["height"], adult_howell["weight"], label="over 18")
+        plt.scatter(child_howell["height"], child_howell["weight"], label="under 18")
+
+        plt.xlabel("Height (cm)")
+        plt.ylabel("Weight (kg)")
+
+        plt.legend()
+
+        return plt.gca()
+
+
+    # _()
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell(column=1, hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Lecture
+    """)
+    return
+
+
+@app.cell
+def _(SEX, az, howell_adults, pl, plt):
+    def plot_howell_HW_dist():
+        fig, axs = plt.subplots(1, 3, figsize=(15, 4))
+
+        plt.sca(axs[0])
+        for sex_idx, label in enumerate(SEX):
+            plt.scatter(
+                howell_adults.filter(pl.col("male") == sex_idx)["height"],
+                howell_adults.filter(pl.col("male") == sex_idx)["weight"],
+                label=label,
+                alpha=0.7,
+                s=50,
+            )
+        plt.xlabel("Height (cm)")
+        plt.ylabel("Weight (kg)")
+        plt.title("Weight vs Height")
+        plt.legend(title="Sex")
+
+        for var_idx, col in enumerate(["height", "weight"]):
+            plt.sca(axs[var_idx + 1])
+            for sex_idx2, sex in enumerate(SEX):
+                az.plot_dist(
+                    howell_adults.filter(howell_adults["male"] == sex_idx2)[col],
+                    label=sex,
+                    color=f"C{sex_idx2}",
+                )
+            plt.ylabel(f"{col}".capitalize())
+            plt.title(f"Dist. of {col.capitalize()} Split by Sex")
+        return plt.gca()
+
+
+    plot_howell_HW_dist()
+    return
+
+
+@app.cell
+def _(np, rng):
+    a = np.array([1] * 5 + [0] * 3)
+    b = np.array([0, 5])
+    a, b, b[a]
+
+    b, rng.binomial(1, 0.5, 4), b[rng.binomial(1, 0.5, 4)], b[a]
+    return
+
+
+@app.cell(column=2, hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Chapter Notes
+    """)
+    return
+
+
+@app.cell
+def _(plt, waffle_data):
     _, _ax = plt.subplots(1, 2, figsize=(14, 5))
 
-    _ax[0].scatter(data["Marriage"], data["Divorce"])
+    _ax[0].scatter(waffle_data["marriage"], waffle_data["divorce"])
     _ax[0].set_xlabel("Marriage Rate")
     _ax[0].set_ylabel("Divorce Rate")
     _ax[0].set_title("Divorce vs Marriage Rate")
 
-    _ax[1].scatter(data["MedianAgeMarriage"], data["Divorce"])
+    _ax[1].scatter(waffle_data["medianAgeMarriage"], waffle_data["divorce"])
     _ax[1].set_xlabel("Median Age Marriage")
-    _ax[1].set_ylabel("Diverce Rate")
+    _ax[1].set_ylabel("Divorce Rate")
     _ax[1].set_title("Marriage vs Median Age of Marriage")
     return
 
 
 @app.cell
-def _(data, np, plt, pm):
-    with pm.Model() as m5_1:
-        _a = pm.Normal("a", 0, 0.2)
-        _bA = pm.Normal("bA", 0, 0.5)
-        _sigma = pm.Exponential("sigma", 1)
-        _mu = _a + _bA * data.get_column("MedianAgeMarriage_std").to_numpy()
+def _(np, pm, waffle_data):
+    def m5_1(draws: int = 50):
+        with pm.Model() as m5_1:
+            a = pm.Normal("a", 0, 0.2)
+            bA = pm.Normal("bA", 0, 0.5)
+            sigma = pm.Exponential("sigma", 1)
+            mu = a + bA * waffle_data.get_column("medianAgeMarriage_std").to_numpy()
 
-        _div_rate = pm.Normal(
-            "divorce_rate",
-            mu=_mu,
-            sigma=_sigma,
-            observed=data["Divorce_std"].to_numpy(),
+            div_rate = pm.Normal(
+                "divorce_rate",
+                mu=mu,
+                sigma=sigma,
+                observed=waffle_data["divorce_std"].to_numpy(),
+            )
+
+            prior_samples = pm.sample_prior_predictive(draws)
+
+            idata = pm.sample(draws)
+
+        x = np.linspace(
+            waffle_data["medianAgeMarriage_std"].min(), waffle_data["medianAgeMarriage_std"].max()
         )
 
-        _prior_samples = pm.sample_prior_predictive()
-
-        m5_1_idata = pm.sample(1000)
-
-    _x = np.linspace(data["MedianAgeMarriage_std"].min(), data["MedianAgeMarriage_std"].max())
-
-    _a_plot = _prior_samples["prior"]["a"].to_numpy().flatten()
-    _bA_plot = _prior_samples["prior"]["bA"].to_numpy().flatten()
-    _mu_plot = _a_plot[:, None] + _bA_plot[:, None] * _x
-
-    plt.plot(_x, _mu_plot.T, c="g", alpha=0.4)
-    return (m5_1_idata,)
+        return prior_samples, idata, x
 
 
-@app.cell
-def _(data):
-    data.select(["MedianAgeMarriage", "Divorce", "Marriage"]).corr()
+    # m5_1_prior, m5_1_idata, m5_1_x = m5_1(draws=50)
+
+    # a_plot = m5_1_prior["prior"]["a"].to_numpy().flatten()
+    # bA_plot = m5_1_prior["prior"]["bA"].to_numpy().flatten()
+    # mu_plot = a_plot[:, None] + bA_plot[:, None] * m5_1_x
+
+    # plt.plot(m5_1_x, mu_plot.T, c="g", alpha=0.4)
     return
 
 
 @app.cell
-def _(az, data, m5_1_idata, np, plt):
+def _(waffle_data):
+    waffle_data.select(["medianAgeMarriage", "divorce", "marriage"]).corr()
+    return
+
+
+@app.cell
+def _(az, m5_1_idata, np, plt, waffle_data):
     _a_samples = (
         m5_1_idata["posterior"]["a"].to_numpy().flatten()
     )  # shape is (4 * n_samples,)
@@ -122,24 +328,24 @@ def _(az, data, m5_1_idata, np, plt):
         m5_1_idata["posterior"]["bA"].to_numpy().flatten()
     )  # shape is (4 * n_samples,)
     _x = np.linspace(
-        data["MedianAgeMarriage_std"].min(),
-        data["MedianAgeMarriage_std"].max(),
-        data.shape[0],
+        waffle_data["medianAgeMarriage_std"].min(),
+        waffle_data["medianAgeMarriage_std"].max(),
+        waffle_data.shape[0],
     )
 
     # Raw data
-    plt.scatter(data["MedianAgeMarriage"], data["Divorce"], c="red", label="data")
+    plt.scatter(waffle_data["medianAgeMarriage"], waffle_data["divorce"], c="red", label="waffle_data")
 
     # MAP - convert back to original scale
-    _divorce_mean = data["Divorce"].mean()
-    _divorce_std = data["Divorce"].std()
+    _divorce_mean = waffle_data["divorce"].mean()
+    _divorce_std = waffle_data["divorce"].std()
 
     _map_line = (
-        _a_samples.mean() + _bA_samples.mean() * data["MedianAgeMarriage_std"].to_numpy()
+        _a_samples.mean() + _bA_samples.mean() * waffle_data["medianAgeMarriage_std"].to_numpy()
     ) * _divorce_std + _divorce_mean
 
     plt.plot(
-        data["MedianAgeMarriage"],
+        waffle_data["medianAgeMarriage"],
         _map_line,
         c="green",
         lw=3,
@@ -151,13 +357,13 @@ def _(az, data, m5_1_idata, np, plt):
     # Result: (n_samples, n_points)
     _mu_std = (
         _a_samples[:, np.newaxis]
-        + _bA_samples[:, np.newaxis] * data["MedianAgeMarriage_std"].to_numpy()
+        + _bA_samples[:, np.newaxis] * waffle_data["medianAgeMarriage_std"].to_numpy()
     )
     _mu = _mu_std * _divorce_std + _divorce_mean
 
     # 89% HDI mean
     _mu_hdi = az.hdi(_mu, hdi_prob=0.89)
-    az.plot_hdi(x=data["MedianAgeMarriage"].to_numpy(), hdi_data=_mu_hdi, color="green")
+    az.plot_hdi(x=waffle_data["medianAgeMarriage"].to_numpy(), hdi_data=_mu_hdi, color="green")
 
     plt.xlabel("MedianAgeMarriage")
     plt.ylabel("Divorce")
@@ -169,11 +375,6 @@ def _(az, data, m5_1_idata, np, plt):
 @app.cell
 def _(az, m5_1_idata):
     az.summary(m5_1_idata)
-    return
-
-
-@app.cell(column=1)
-def _():
     return
 
 
---
+++ Ch_5_rethinking_stats/marcelo_fischer_week_5.py
@@ -0,0 +1,96 @@
+import marimo
+
+__generated_with = "0.20.4"
+app = marimo.App(width="columns")
+
+
+@app.cell(column=0)
+def _():
+    import arviz as az
+    import marimo as mo
+    import matplotlib.pyplot as plt
+    import altair as alt
+    import numpy as np
+    import polars as pl
+    import pymc as pm
+    import scipy as sp
+    from scipy.stats import beta, binom, norm
+
+    RANDOM_SEED = 1523
+    rng = np.random.default_rng(RANDOM_SEED)
+
+    az.style.use("fivethirtyeight")
+    return pl, plt
+
+
+@app.cell
+def _(pl):
+    SEX = ["women", "men"]
+
+    df = pl.read_csv(
+        "/home/marcelo/git/rethinking_stats_and_PyMC/data/Howell1.csv", separator=";"
+    )
+
+
+    def filter_age_lt(df: pl.DataFrame, age: int) -> pl.DataFrame:
+        return df.filter(pl.col("age") >= age)
+
+
+    data = df.pipe(filter_age_lt, age=18)
+
+    data
+    return SEX, data
+
+
+@app.cell
+def _(SEX, data, pl, plt):
+    def _() -> plt.Axes:
+        # Split by the Sex Category
+        for i, sex in enumerate(SEX):
+            plt.scatter(
+                data.filter(pl.col("male") == i)["weight"].to_numpy(),
+                data.filter(pl.col("male") == i)["height"].to_numpy(),
+                c=f"C{i}",
+                label=sex,
+            )
+
+        plt.legend()
+        plt.xlabel("Height (cm)")
+        plt.ylabel("Weight (kg)")
+        plt.title("Weight vs Height pre Sex")
+
+        # graph = mo.ui.matplotlib(plt.gca())
+        return plt.gca()
+
+
+    _()
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell(column=1)
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
